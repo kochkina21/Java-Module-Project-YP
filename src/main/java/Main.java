298604a5ad900cdea2dff68e0b37cd78b4c61864
DuchessKoch <kochkina21@yandex.ru>
2025-03-12 21:31:11 +0300

    Практическая работа ver.2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,14 +14,15 @@ public class Main {
             name = scanner.next();
 
             while (true) {
-                System.out.println("Введите скорость автомобиля №" + i + " от 0 до 250 км/ч");
                 if (scanner.hasNextInt()) {
                     speed = scanner.nextInt();
                     if (speed > 0 && speed <= 250) {
                         break;
                     }
                 } else {
-                    System.out.println("Введите целочисленное значение скорости");
+                    System.out.println("Ошибка! Скорость должна быть от 0 до 250 км/ч.");
+                } else {
+                    System.out.println("Ошибка! Скорость должна быть целочисленным значением.");
                 }
             }
 
